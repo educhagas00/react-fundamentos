f44b7c87f44b257ccc5de1d7a84aa194b50a004d
Eduardo <educhagas00@gmail.com>
2025-03-14 21:31:13 -0300

    add: renderizando listas

--- src/App.js
+++ src/App.js
@@ -2,6 +2,12 @@ import React from 'react';
 import Post from './Post';
 import Header from './Header';
 
+const posts = [
+  { title: 'Title#01', subtitle: 'Sub#01' , likes: 20 },
+  { title: 'Title#02', subtitle: 'Sub#02' , likes: 10 },
+  { title: 'Title#03', subtitle: 'Sub#03' , likes: 50 },
+];
+
 function App() {
   return (  // <>  short syntax para React.Fragment. Componente que não renderiza nada, mas serve para agrupar elementos
   <> 
@@ -11,29 +17,16 @@ function App() {
 
     <hr />
 
-    <Post
-      likes={20}
-      post={{
-        title: "titulo da notícia 01",
-        subtitle: "subtitulo da notícia 01",
-      }}
-    />
-
-    <Post
-      likes={10}
-      post={{
-        title: "titulo da notícia 02",
-        subtitle: "subtitulo da notícia 02",
-      }}
-    />
-
-    <Post
-    likes={50}
-      post={{
-        title: "titulo da notícia 03",
-        subtitle: "subtitulo da notícia 03",
-      }}
-    />
+    {posts.map(post => (    
+      <Post
+        key={post.title}
+        likes={post.likes}
+        post={{
+          title: post.title,
+          subtitle: post.subtitle,
+        }} 
+      />
+    ))}
   </>  
   ); // componentes irmaos precisam estar dentro de um componente pai (nesse caso, o fragment)
 }
