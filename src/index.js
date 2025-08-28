c7fdfa9543744e03335e451893ec6f1c7c555295
Eduardo <educhagas00@gmail.com>
2025-08-27 22:31:01 -0300

    Global style component

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+import GlobalStyle from './styles/global.js'; 
+
 import App from './components/App';
 
 const root = ReactDOM.createRoot(document.getElementById('root')); // cria a raiz do React a partir do id root lá no index.html da pasta public
 
 // renderiza o conteúdo na raiz
 root.render(
-<App />
+  <>
+    <GlobalStyle />
+    <App />
+  </>
 );
