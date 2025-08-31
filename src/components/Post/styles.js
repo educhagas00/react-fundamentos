717cce6fbdfc2e7b9b37056472ad8c2a76adb02e
Eduardo <educhagas00@gmail.com>
2025-08-30 22:09:18 -0300

    Post styled component

--- src/components/Post/styles.js
+++ src/components/Post/styles.js
@@ -1,5 +1,12 @@
 import styled from 'styled-components';
 
+export const Container = styled.article`
+  margin-bottom: 24px;
+
+  opacity: ${(props) => props.removed ? 0.7 : 1};
+  color: ${(props) => props.removed ? 'red' : '#fff'};
+`;
+
 export const Subtitle = styled.small`
   display: block;
 `;
