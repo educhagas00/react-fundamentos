5f54d5f47aadda42b4e69e4cbd598d774ccf95c5
Eduardo <educhagas00@gmail.com>
2025-09-03 02:24:25 -0300

    Header and Footer component/styled component

---
+++ src/components/Footer/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+export default function Footer() { 
+  return (
+    <Container>
+      <span>Dudu's Blog. Todos os direitos reservados.</span>
+      <button type="button">🌞</button>
+    </Container>
+  );
+}
+
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,33 +1,13 @@
-import React, { use, useContext } from 'react';
-import PropTypes from 'prop-types';
-import Button from '../Button';
+import React from 'react';
+import { Container } from './styles';
 
-import { ThemeContext } from '../../context/ThemeContext'; 
-import Title from '../Title';
-
-export default function Header(props) { // desestruturação de props 
-
-  const { onToggleTheme } = useContext(ThemeContext); 
+export default function Header() { 
 
   return (
-    <>
-      <Title>{props.title}</Title>
-      <Button onClick={onToggleTheme}>
-        Mudar Tema
-      </Button>
-      {props.children} 
-    </>
+    <Container>
+      <h1>Dudu's Blog</h1>
+      <button type="button">🌞</button>
+    </Container>
   );
 }
 
-// children é uma propriedade especial do React que permite renderizar o conteúdo que está dentro do componente
-// children vai receber tudo que esta dentro do componente/tag Header
-
-Header.propTypes = { 
-  title: PropTypes.string, 
-  children: PropTypes.node, 
-};
-
-Header.defaultProps = {
-  title: `JStack's Blog`,
-};
