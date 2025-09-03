1d6dc31067ee859fa31e86cba9f94e454604f246
Eduardo <educhagas00@gmail.com>
2025-09-03 02:48:41 -0300

    Layout component & App render

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Layout from './components/Layout';
+import GlobalStyles from './styles/global';
+
+export default function App() {
+  return (
+    <>
+      <GlobalStyles />
+      <Layout />
+    </>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -3,14 +3,11 @@ import ReactDOM from 'react-dom/client';
 
 import GlobalStyle from './styles/global.js'; 
 
-import App from './components/App';
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root')); // cria a raiz do React a partir do id root lá no index.html da pasta public
 
 // renderiza o conteúdo na raiz
 root.render(
-  <>
-    <GlobalStyle />
     <App />
-  </>
 );
